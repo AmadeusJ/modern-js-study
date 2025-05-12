c0b8c265befcb870551138c3f551cb5863dd20ae
dawoonjung <dawoonjung1026@gmail.com>
2025-05-12 14:05:49 +0900

    nuxt study

---
+++ udemy/nuxt/car-trader/pages/car/[name]-[id].vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  const route = useRoute();
+  const { toTitleCase } = useUtilities();
+  useHead({
+    title: toTitleCase(route.params.name),
+  });
+
+  definePageMeta({
+    layout: "custom",
+  });
+</script>
+
+<template>
+  <div
+    class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+  >
+    <!-- CAR HERO -->
+    <CarDetailHero />
+    <!-- CAR HERO -->
+    <!-- CAR ATTRIBUTES -->
+    <CarDetailAttribute />
+    <!-- CAR ATTRIBUTES -->
+    <!-- CAR DESCRISPTION -->
+    <CarDetailDescription />
+    <!-- CAR DESCRISPTION -->
+    <!-- CAR CONTACT -->
+    <CarDetailContact />
+    <!-- CAR CONTACT -->
+  </div>
+</template>
